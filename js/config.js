90f2f8516727437aae4999115cde3dfcd27e8c4b
河野大輔 <kounodaisuke@kounodaisukenoMacBook-Air.local>
2026-05-13 13:59:18 +0900

    GAS Web App URLを設定

--- js/config.js
+++ js/config.js
@@ -2,4 +2,4 @@
 // GAS Web App URL設定
 // デプロイ後にこのURLを更新してください
 // ============================================================
-const GAS_URL = 'YOUR_GAS_WEB_APP_URL_HERE';
+const GAS_URL = 'https://script.google.com/macros/s/AKfycbxbCWCFI1nEW9z9ZJz_sDNxXOFaED2d0f_VP1Pgy2JXrnZdwycLrvoMkKWpMmWVu1qX/exec';
